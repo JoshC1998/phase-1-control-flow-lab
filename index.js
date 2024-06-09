5cea69f1eea6609505affb6f0b296e1f1335f041
Joshua Cruz <jcruz1998@gmail.com>
2024-06-09 15:51:46 -0400

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance) {
+  if(distance <= 400) {
+    return 'This one is on me!'};
+  if(distance > 400 && distance < 2000) {
+    return 'That will be twenty bucks.'};
+  if(distance > 2000 && distance < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  };
+  if(distance > 2500) {
+    return 'No can do.'
+  }
+  }
+
+function ternaryCheckCity(city){
+  if(city ==='NYC'){
+    return "Ok, sounds good."
+  }
+  else if(city !== 'NYC'){
+    return "No go."
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous': 
+      return 'Thank you.';
+    default : 
+      return 'Bye.'
+  }}
+  
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+ 
